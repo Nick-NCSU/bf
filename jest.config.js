a24c815572a1083cb198f0ddaec6288cc2364225
nathomp3 <nathomp3@ncsu.edu>
2024-05-23 20:37:08 -0400

    Fix test failures

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,11 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: 'ts-jest/presets/js-with-ts',
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
+  },
+  modulePaths: ['<rootDir>/src'],
 };
